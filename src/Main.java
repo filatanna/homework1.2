62d693dff12c9296c79763fdb2f56118cebc34ca
User <filataanna@mail.ru>
2023-01-02 17:13:34 +0300

    задача 6-8

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) {
       task1();
@@ -8,9 +10,26 @@ public class Main {
     }
 
     private static void task5() {
+        var timeWork = 640;
+        var timeWorkOne = 8;
+        var personal = timeWork / timeWorkOne;
+        System.out.println("Всего работников в компании - "+ personal + " человек");
+        personal = personal + 94;
+        System.out.println(personal);
+        timeWork = personal * timeWorkOne;
+        System.out.println("Если в компании работает " + personal + " человек, то всего " + timeWork + " часов работы может быть поделено между сотрудниками");
+
     }
 
     private static void task4() {
+        var boxerOne = 78.2;
+        var boxerTwo = 87.2;
+        System.out.println(boxerOne+boxerTwo);
+        System.out.println(boxerTwo-boxerOne);
+        var raznica1 = boxerTwo-boxerOne;
+        System.out.println(raznica1);
+        var raznica2 = boxerTwo % boxerOne;
+        System.out.println(raznica2);
     }
 
     private static void task3() {
