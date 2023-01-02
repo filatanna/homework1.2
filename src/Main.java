f925fea7331ece11e18cef179332f93af2b1739f
User <filataanna@mail.ru>
2023-01-02 16:45:47 +0300

    задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      task1();
+      task2();
+      task3();
+      task4();
+      task5();
+    }
+
+    private static void task5() {
+    }
+
+    private static void task4() {
+    }
+
+    private static void task3() {
+    }
+
+    private static void task2() {
+    }
+
+    private static void task1() {
+        var dog = 8.0;
+        var cat = 3.0;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
     }
 }
